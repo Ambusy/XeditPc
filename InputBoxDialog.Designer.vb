10cf8712cdd2a94d33c623b19f6c28b14b696943
Ambusy <Ambusy@users.noreply.github.com>
2017-11-22 19:10:30 +0200

    replaced VB's InputBox with my own InputBox, with a CANCEL option

---
+++ InputBoxDialog.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InputBoxDialog
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Says = New System.Windows.Forms.Label()
+        Me.Response = New System.Windows.Forms.TextBox()
+        Me.OkB = New System.Windows.Forms.Button()
+        Me.CancelB = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Says
+        '
+        Me.Says.AutoSize = True
+        Me.Says.Location = New System.Drawing.Point(12, 9)
+        Me.Says.Name = "Says"
+        Me.Says.Size = New System.Drawing.Size(39, 13)
+        Me.Says.TabIndex = 0
+        Me.Says.Text = "Label1"
+        '
+        'Response
+        '
+        Me.Response.Location = New System.Drawing.Point(15, 204)
+        Me.Response.Name = "Response"
+        Me.Response.Size = New System.Drawing.Size(510, 20)
+        Me.Response.TabIndex = 1
+        '
+        'OkB
+        '
+        Me.OkB.Location = New System.Drawing.Point(423, 239)
+        Me.OkB.Name = "OkB"
+        Me.OkB.Size = New System.Drawing.Size(102, 20)
+        Me.OkB.TabIndex = 2
+        Me.OkB.Text = "OK"
+        Me.OkB.UseVisualStyleBackColor = True
+        '
+        'CancelB
+        '
+        Me.CancelB.Location = New System.Drawing.Point(15, 230)
+        Me.CancelB.Name = "CancelB"
+        Me.CancelB.Size = New System.Drawing.Size(102, 20)
+        Me.CancelB.TabIndex = 0
+        Me.CancelB.TabStop = False
+        Me.CancelB.Text = "Cancel"
+        Me.CancelB.UseVisualStyleBackColor = True
+        '
+        'InputBoxDialog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(539, 262)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.CancelB)
+        Me.Controls.Add(Me.OkB)
+        Me.Controls.Add(Me.Response)
+        Me.Controls.Add(Me.Says)
+        Me.Name = "InputBoxDialog"
+        Me.Text = "InputBoxDialog"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Says As System.Windows.Forms.Label
+    Friend WithEvents Response As System.Windows.Forms.TextBox
+    Friend WithEvents OkB As System.Windows.Forms.Button
+    Friend WithEvents CancelB As System.Windows.Forms.Button
+End Class
---
+++ InputBoxDialog.vb
@@ -0,0 +1,33 @@
+﻿Public Class InputBoxDialog
+    ' on INPUT:
+    Friend InpTit As String ' title
+    Friend InpSay As String ' display text
+    ' on RETURN:
+    Friend OutResp As String ' answer (on entry contains default)
+    Friend OKBut As Boolean ' which button
+    Private Sub CancelB_Click(sender As Object, e As EventArgs) Handles CancelB.Click
+        OKBut = False
+        Me.Visible = False
+    End Sub
+    Private Sub OkB_Click(sender As Object, e As EventArgs) Handles OkB.Click
+        OKBut = True
+        If Response.Text.Length > 0 Then
+            OutResp = Response.Text
+        End If
+        Me.Visible = False
+    End Sub
+    Private Sub InputBoxDialog_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Me.Text = InpTit
+        Says.Text = InpSay
+        Response.Text = ""
+        Response.Focus()
+        Me.Refresh()
+    End Sub
+
+    Private Sub Response_KeyDown(sender As Object, e As KeyEventArgs) Handles Response.KeyDown
+        If e.KeyCode = 13 Then
+            OkB_Click(sender, New EventArgs())
+        End If
+
+    End Sub
+End Class
