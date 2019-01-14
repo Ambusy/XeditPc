6a93177f71840a7fc1c9de0d67d91e19bf591393
Ambusy <Ambusy@users.noreply.github.com>
2019-01-14 17:28:03 +0200

    Add files via upload

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("XeditPc")> 
+<Assembly: AssemblyDescription("IBM's mainframe Editor for Windows (for .Net 3.5)")> 
+<Assembly: AssemblyCompany("AMBusy")> 
+<Assembly: AssemblyProduct("XeditPc")> 
+<Assembly: AssemblyCopyright("Copyright  ©  2007  -  2017     AMBusy @ Gmail.com")> 
+<Assembly: AssemblyTrademark("IBM is trademark of Int. Buss. Machines")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3323ee4c-814a-4944-9d00-f9e18ba923e5")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("2.2.1.9")> 
+<Assembly: AssemblyFileVersion("2.2.1.9")> 
